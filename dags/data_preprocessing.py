a78267b7d7a1575bfc7a5912365e1ca3a901c8fe
dhananjay_pc <dhananjayakumarmittapalli@gmail.com>
2024-11-03 10:37:01 -0500

    minor fixes

--- dags/data_preprocessing.py
+++ dags/data_preprocessing.py
@@ -4,7 +4,6 @@ from airflow.operators.dummy import DummyOperator
 from airflow.operators.python import PythonOperator
 from datetime import datetime
 from airflow import configuration as conf
-# from src.data_preprocessing.data_preprocessing import preprocess_image\
 
 import os
 import sys
@@ -20,6 +19,13 @@ from src.preprocessing import process_images
 # Enable pickle support for XCom
 conf.set('core', 'enable_xcom_pickling', 'True')
 
+# Set up custom file logger
+PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+LOG_DIR = os.path.join(PROJECT_DIR, "logs")
+os.makedirs(LOG_DIR, exist_ok=True)
+LOG_FILE_PATH = os.path.join(LOG_DIR, 'airflow.log')
+
+DAG_NAME = 'Data_Preprocessing_Steps'
 
 
 original_data_folder = '/content/drive/My Drive/MLOPs Project/sampled_data'
@@ -30,14 +36,14 @@ label_json_path = '/content/drive/My Drive/MLOPs Project/label_indices.json'
 default_args = {
     'owner': 'MLOPs_Team10',
     'depends_on_past': False,
-    'start_date': datetime(2022, 1, 1),
+    'start_date': datetime(2024, 10, 11),
     'email_on_failure': False,
     'email_on_retry': False,
     'retries': 1
 }
 
 dag = DAG(
-    'Data_Preprocessing_Steps',
+    DAG_NAME,
     default_args=default_args,
     description='Data preprocessing Steps of Images',
     schedule_interval=None,  # Set the schedule interval or use None for manual triggering

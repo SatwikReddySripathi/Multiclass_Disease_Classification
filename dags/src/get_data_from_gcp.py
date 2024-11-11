7bbd692dfe9d246d3f3652af320450fdf07aaf12
Dhanush Akula <77378367+DhanushAkula@users.noreply.github.com>
2024-11-11 16:19:18 -0500

    Update get_data_from_gcp.py

--- dags/src/get_data_from_gcp.py
+++ dags/src/get_data_from_gcp.py
@@ -8,47 +8,6 @@ import json
 from google.cloud import storage
 import pickle
 from PIL import Image
-import logging
-from airflow.utils.log.logging_mixin import LoggingMixin
-
-# Set up Airflow logger
-airflow_logger = LoggingMixin().log
-# Set the project directory
-PROJECT_DIR = '/opt/airflow'
-LOG_DIR = os.path.join(PROJECT_DIR, "logs")
-os.makedirs(LOG_DIR, exist_ok=True)
-LOG_FILE_PATH = os.path.join(LOG_DIR, 'data_extraction.log')
-
-
-logger = logging.getLogger('file_logger')
-logger.setLevel(logging.DEBUG)  # Setting to DEBUG to capture all log messages or else it might not log info and error messages
-
-file_handler = logging.FileHandler(LOG_FILE_PATH)
-file_handler.setLevel(logging.DEBUG)
-
-console_handler = logging.StreamHandler()
-console_handler.setLevel(logging.DEBUG)
-
-formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-file_handler.setFormatter(formatter)
-console_handler.setFormatter(formatter)
-
-logger.addHandler(file_handler)
-logger.addHandler(console_handler)
-
-logger.info("Logging configuration is set. Logs will be saved to: {}".format(LOG_FILE_PATH))
-
-def custom_log(message, level=logging.INFO):
-    """Log to both Airflow and custom file logger"""
-    if level == logging.INFO:
-        airflow_logger.info(message)
-        logger.info(message)
-    elif level == logging.ERROR:
-        airflow_logger.error(message)
-        logger.error(message)
-    elif level == logging.WARNING:
-        airflow_logger.warning(message)
-        logger.warning(message)
 
 
 def extract_md5_from_dvc(file_path):
@@ -60,7 +19,7 @@ def extract_md5_from_dvc(file_path):
             return data.get("outs", [{}])[0].get("md5")
     except Exception as e:
         print(f"Error reading {file_path}: {e}")
-        custom_log(f"Error reading {file_path}: {e}",level=logging.ERROR)
+        #custom_log(f"Error reading {file_path}: {e}",level=logging.ERROR)
         return None
 
 def find_md5_hashes(project_dir):
@@ -74,10 +33,10 @@ def find_md5_hashes(project_dir):
         if md5_hash:
             md5_keys.append(md5_hash)
             print(f"MD5 hash for {os.path.basename(dvc_file)}: {md5_hash}")
-            custom_log(f"MD5 hash for {os.path.basename(dvc_file)}: {md5_hash}")
+            #custom_log(f"MD5 hash for {os.path.basename(dvc_file)}: {md5_hash}")
         else:
             print(f"No MD5 hash found in {os.path.basename(dvc_file)} or failed to extract.")
-            custom_log(f"No MD5 hash found in {os.path.basename(dvc_file)} or failed to extract.",level=logging.ERROR)
+            #custom_log(f"No MD5 hash found in {os.path.basename(dvc_file)} or failed to extract.",level=logging.ERROR)
 
     return md5_keys
 
@@ -91,7 +50,7 @@ def get_file_contents_as_dict(bucket, md5_keys):
         for md5_key in md5_keys:
             if md5_key[2:] == blob_name:
                 print(f'Reading content from {blob.name}...')
-                custom_log(f'Reading content from {blob.name}...')
+                #custom_log(f'Reading content from {blob.name}...')
                 content = blob.download_as_text()  # Read the blob content as text
                 
                 # Check the file type based on the extension
@@ -100,18 +59,18 @@ def get_file_contents_as_dict(bucket, md5_keys):
                     try:
                         json_content_dict[md5_key] = json.loads(content)
                         print(f"JSON content loaded for {blob.name}.")
-                        custom_log(f"JSON content loaded for {blob.name}.")
+                        #custom_log(f"JSON content loaded for {blob.name}.")
                     except json.JSONDecodeError:
                         print(f"Error decoding JSON content in {blob.name}.")
-                        custom_log(f"Error decoding JSON content in {blob.name}.",level=logging.ERROR)
+                        #custom_log(f"Error decoding JSON content in {blob.name}.",level=logging.ERROR)
                 else:
                     # Parse CSV-like content
                     print(f"Parsing CSV content for {blob.name}...")
-                    custom_log(f"Parsing CSV content for {blob.name}...")
+                    #custom_log(f"Parsing CSV content for {blob.name}...")
                     csv_reader = csv.DictReader(StringIO(content))
-                    csv_data = {row['Image Index']: row['Labels'] for row in csv_reader}
+                    csv_data = {row['Image Index']: [row['Labels'],row['Age'],row['Gender']] for row in csv_reader}
                     print(f"CSV content loaded for {blob.name}.")
-                    custom_log(f"CSV content loaded for {blob.name}.")
+                    #custom_log(f"CSV content loaded for {blob.name}.")
     
     return json_content_dict, csv_data
 
@@ -124,13 +83,17 @@ def create_final_json(json_content_dict, csv_content_dict):
             
             # Check if there's a matching label in the CSV content
             if relpath in csv_content_dict.keys():
-                image_label = csv_content_dict[relpath]
+                image_label = csv_content_dict[relpath][0]
+                Age = csv_content_dict[relpath][1]
+                Gender = csv_content_dict[relpath][2]
                 
                 # Create the final JSON structure for each matched entry
                 final_data.append({
                     'md5': item['md5'],
                     'image_index': relpath,
-                    'image_label': image_label
+                    'image_label': image_label,
+                    'Age': Age,
+                    'Gender': Gender
                 })
                 
     return final_data
@@ -143,6 +106,8 @@ def download_and_compress_images(bucket, md5_image_data, output_pickle_file):
         md5 = item["md5"]
         image_index = item["image_index"]
         image_label = item["image_label"]
+        image_Age = item["Age"]
+        image_Gender = item["Gender"]
 
         # Attempt to download the image from GCP bucket
         blob = bucket.blob(f'files/md5/{md5[:2]}/{md5[2:]}')
@@ -158,7 +123,7 @@ def download_and_compress_images(bucket, md5_image_data, output_pickle_file):
             if image.mode in ['RGB','RGBA']:
                 image = image.convert('L')
                 print(f"Converted {image_index}  to L for JPEG compatibility.")
-                custom_log(f"Converted {image_index}  to L for JPEG compatibility.")
+                #custom_log(f"Converted {image_index}  to L for JPEG compatibility.")
             
             # Compress the image
             compressed_image = io.BytesIO()
@@ -166,18 +131,22 @@ def download_and_compress_images(bucket, md5_image_data, output_pickle_file):
             compressed_image.seek(0)  # Rewind the buffer
             
             # Store compressed image in dictionary
-            compressed_images[image_index] = {'image_data': compressed_image.getvalue(), 'image_label': image_label}
+            compressed_images[image_index] = {'image_data': compressed_image.getvalue(), 'image_label': image_label,'Age': image_Age,'Gender': image_Gender}
             print(f"Compressed and stored image: {image_index}")
-            custom_log(f"Compressed and stored image: {image_index}")
+            #custom_log(f"Compressed and stored image: {image_index}")
 
         except Exception as e:
             print(f"Failed to download or compress image {image_index} with MD5 {md5}: {e}")
-            custom_log(f"Failed to download or compress image {image_index} with MD5 {md5}: {e}",level=logging.ERROR)
+            #custom_log(f"Failed to download or compress image {image_index} with MD5 {md5}: {e}",level=logging.ERROR)
 
     # Save all compressed images in a pickle file
     with open(output_pickle_file, 'wb') as f:
         pickle.dump(compressed_images, f)
         print(f"All compressed images saved to {output_pickle_file}")
-        custom_log(f"All compressed images saved to {output_pickle_file}")
+        #custom_log(f"All compressed images saved to {output_pickle_file}")
+
+
+
+
 
 

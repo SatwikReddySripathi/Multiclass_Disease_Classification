6dd6277a801708a97566ff6e07f84d872b0d296e
Dhanush Akula <77378367+DhanushAkula@users.noreply.github.com>
2024-11-15 03:47:27 -0500

    Update model_development_refined.py

--- model/model_development_refined.py
+++ model/model_development_refined.py
@@ -228,7 +228,6 @@ def evaluate_model(test_loader, model, criterion, precision_metric, recall_metri
 
 if __name__ == "__main__":
 PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-file_path = 
   config = {
       "file_path": os.path.join(PROJECT_DIR, "model", "preprocessed_dummy_data.pkl")
       "batch_size": 32,

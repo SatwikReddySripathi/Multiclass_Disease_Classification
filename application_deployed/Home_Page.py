dbe31f50db6d46ce4e704a8dce87dd9f49d3c04f
Satwik Reddy Sripathi <satwikreddysripathi@gmail.com>
2024-12-09 08:39:24 -0500

    Update Home_Page.py

--- application_deployed/Home_Page.py
+++ application_deployed/Home_Page.py
@@ -1,5 +1,5 @@
 import streamlit as st
-#st.image("assets/logo.png", caption="")
+st.image("assets/logo.png", caption="")
 st.markdown(
     """
     <style>
@@ -146,7 +146,7 @@ st.markdown(
             Have questions or need assistance? We’d love to hear from you!
         </p>
         <p style="color: #BB86FC; text-align: center; font-size: 1.2em;">
-            Email us at: <a href="mailto:thoraix.mlops@gmail.com" style="color: #03DAC6; text-decoration: none;">contact@thorai.com</a>
+            Email us at: <a href="mailto:thoraix.mlops@gmail.com" style="color: #03DAC6; text-decoration: none;">thoraix.mlops@gmail.com</a>
         </p>
     </div>
     """,
